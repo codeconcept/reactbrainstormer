33aebbf4e7f81430cade945ae20d9ff365e99e51
codeconcept <contact@codeconcept.fr>
2019-04-11 18:02:02 +0200

    Firestore specific Timestamp introduction

--- src/components/Idea.jsx
+++ src/components/Idea.jsx
@@ -4,7 +4,7 @@ const Idea = ({idea}) => {
   return(
     <div className="idea">
       <div className="idea-title">{idea.title}</div>
-      <div className="idea-date">11 avril 2019</div>
+      <div className="idea-date">{idea.createdAt.toDate().toString()}</div>
       <div>
         {idea.content}
       </div>
