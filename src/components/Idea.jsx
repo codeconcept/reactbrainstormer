87cc88ba1c8734c2eec01bc2c51dac56d144ce84
codeconcept <contact@codeconcept.fr>
2019-04-12 17:19:37 +0200

    formating dates with date-fns

--- src/components/Idea.jsx
+++ src/components/Idea.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import formatDate from 'date-fns/format'
 
 const Idea = ({idea}) => {
   return(
     <div className="idea">
       <div className="idea-title">{idea.title}</div>
-      <div className="idea-date">{idea.createdAt.toDate().toString()}</div>
+      <div className="idea-date">{formatDate(idea.createdAt.seconds * 1000, 'ddd MMM Do HH:mm')}</div>
       <div>
         {idea.content}
       </div>
